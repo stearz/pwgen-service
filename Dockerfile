4697a06d62b2bdb8b6c01ac4b0d93cd49b8ad64f
Stephan Schwarz <schwarz@e-spirit.com>
2021-04-24 13:34:10 +0200

    pleased linter and disabled pytest as we don't have tests at the miment

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ COPY . .
 
 EXPOSE 5000
 
-CMD [ "python", "./pwgen-service.py" ]
+CMD [ "python", "./pwgenService.py" ]
--- pwgenService.py
+++ pwgenService.py
@@ -12,18 +12,20 @@ from flask import request, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
 
+
 # helpers
-def _get_int_arg_from_request(arg,the_request):
+def _get_int_arg_from_request(arg, the_request):
     if arg in the_request.args:
         return int(request.args[arg])
     else:
         return False
 
+
 # API route methods
 @app.route('/', methods=['GET'])
 def home():
     return '''<!DOCTYPE html>
-              <html><head><title>PWGen Service</title></head><body>
+              <html><head><title>pwgenService</title></head><body>
               <h1>PWGen Service</h1>
               <p>Get passwords by sending GET requests to /api/v1/password and include these parameters:</p>
               <p><ul>
@@ -35,20 +37,21 @@ def home():
               <p>The generated passwords are returned in a list in JSON format.</p>
               </body></html>'''
 
+
 @app.route('/api/v1/password', methods=['GET'])
 def api_v1_password():
     results = []    
-    
-    length = _get_int_arg_from_request("length",request)
-    specials = _get_int_arg_from_request("specials",request)
-    numbers = _get_int_arg_from_request("numbers",request)
-    count = _get_int_arg_from_request("count",request)
-    
+
+    length = _get_int_arg_from_request("length", request)
+    specials = _get_int_arg_from_request("specials", request)
+    numbers = _get_int_arg_from_request("numbers", request)
+    count = _get_int_arg_from_request("count", request)
+
     if (length and specials and numbers and count):
         for pw in range(int(count)):
-            pw_numbers  = ''.join(secrets.choice(string.digits) for i in range(numbers))
+            pw_numbers = ''.join(secrets.choice(string.digits) for i in range(numbers))
             pw_specials = ''.join(secrets.choice('!@#$%&*-_=+;:,./?') for i in range(specials))
-            pw_chars    = ''.join(secrets.choice(string.ascii_letters) for i in range(length - (numbers + specials)))
+            pw_chars = ''.join(secrets.choice(string.ascii_letters) for i in range(length - (numbers + specials)))
             
             pw_list = list(pw_numbers + pw_specials + pw_chars)
             random.shuffle(pw_list)
@@ -60,4 +63,6 @@ def api_v1_password():
         results.append("ERROR: You have not set all required parameters")
         return jsonify(results), 400
 
-app.run()
+
+if __name__ == '__main__':
+    app.run()
